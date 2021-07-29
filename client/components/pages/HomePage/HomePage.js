53bfa209234fcafe8aeb63c271d1301a431d39a3
Shreyas Chittlur Ramesh <chittlurramesh.s@northeastern.edu>
2021-07-29 15:42:02 -0400

    ui change

--- client/components/pages/HomePage/HomePage.js
+++ client/components/pages/HomePage/HomePage.js
@@ -1,31 +1,28 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { push } from 'connected-react-router';
-import R from 'ramda';
-
-import Section from 'react-bulma-companion/lib/Section';
-import Container from 'react-bulma-companion/lib/Container';
-import Title from 'react-bulma-companion/lib/Title';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { push } from 'connected-react-router'
+import R from 'ramda'
+import SearchBar from 'material-ui-search-bar'
+import home from '../../../styles/home.scss'
+import Section from 'react-bulma-companion/lib/Section'
+import Container from 'react-bulma-companion/lib/Container'
+import Title from 'react-bulma-companion/lib/Title'
 
 export default function HomePage() {
-  const dispatch = useDispatch();
-  const { user } = useSelector(R.pick(['user']));
+  const dispatch = useDispatch()
+  const { user } = useSelector(R.pick(['user']))
 
   useEffect(() => {
     if (R.isEmpty(user)) {
-      dispatch(push('/login'));
+      dispatch(push('/login'))
     }
-  }, []);
+  }, [])
 
   return (
-    <div className="home-page page">
-      <Section>
-        <Container>
-          <Title size="1">
-            Home Page
-          </Title>
-        </Container>
-      </Section>
+    <div className='home-page page'>
+      <>
+        <img src='https://i.ibb.co/wzX4mzm/food-name.jpg' className='photo' />
+      </>
     </div>
-  );
+  )
 }
