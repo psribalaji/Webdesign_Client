8b273601337107dec0e8aac8c58ffac4930b5acf
NarendraRaj19 <narenrj95@gmail.com>
2021-08-08 11:52:02 -0400

    Update Restaurant Info Api

--- client/store/thunks/user.js
+++ client/store/thunks/user.js
@@ -1,7 +1,7 @@
 import { snakeToCamelCase } from 'json-style-converter/es5';
 import { store as RNC } from 'react-notifications-component';
 
-import { getUser, putUser, putUserPassword } from '_api/user';
+import { getUser, putUser, putUserPassword, putRestaurantInfo } from '_api/user';
 import { updateUser } from '_actions/user';
 
 import { dispatchError } from '_utils/api';
@@ -38,6 +38,29 @@ export const attemptUpdateUser = updatedUser => dispatch =>
 export const attemptUpdatePassword = passwordInfo => dispatch =>
   putUserPassword(passwordInfo)
     .then(data => {
+      RNC.addNotification({
+        title: 'Success!',
+        message: data.message,
+        type: 'success',
+        container: 'top-right',
+        animationIn: ['animated', 'fadeInRight'],
+        animationOut: ['animated', 'fadeOutRight'],
+        dismiss: {
+          duration: 5000,
+        },
+      });
+
+      return data;
+    })
+    .catch(dispatchError(dispatch));
+
+    export const attemptUpdateRestaurantInfo = updatedRestaurant => dispatch =>
+    putRestaurantInfo(updatedRestaurant)
+    
+    .then(data => {
+      console.log(data+"jjjj")
+      dispatch(updateUser(snakeToCamelCase(data.body)));
+
       RNC.addNotification({
         title: 'Success!',
         message: data.message,
